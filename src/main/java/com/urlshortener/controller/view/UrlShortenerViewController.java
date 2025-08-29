38833fa5e0156535d09f847e57fe0f5a9a13c7b9
vita <4studywork@gmail.com>
2025-08-29 13:39:46 +0200

    add thymeleaf

---
+++ src/main/java/com/urlshortener/controller/view/UrlShortenerViewController.java
@@ -0,0 +1,40 @@
+package com.urlshortener.controller.view;
+
+import com.urlshortener.service.UrlShortener;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.view.RedirectView;
+
+import java.net.URI;
+
+@Controller
+@AllArgsConstructor
+public class UrlShortenerViewController {
+    private final UrlShortener urlShortener;
+
+
+    @GetMapping("/")
+    public String home(Model model,
+                       @RequestParam(required = false) String message) {
+        model.addAttribute("message", message);
+        return "url_form";
+    }
+
+    @PostMapping("/shorten")
+    @ResponseBody
+    public String shortenUrlAjax(@RequestParam String longUrl) {
+        return urlShortener.shorten(longUrl);
+    }
+
+    @PostMapping("/retrieve")
+    @ResponseBody
+    public String retrieveUrlAjax(@RequestParam String shortUrl) {
+        return urlShortener.retrieve(shortUrl)
+                .orElse("Short URL not found");
+    }
+}
